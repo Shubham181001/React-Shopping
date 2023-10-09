99936716f6caa1ce4d5db69e2fb1e6b0562dce5e
Shubham Agarwal <shubham9588986203@gmail.com>
2023-10-09 19:01:23 +0530

    Added Files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import App from "./App";
+import { NetflixRegisterComponent } from "./components/NetflixRegisterComponent";
+import NetflixIndexComponent from "./components/NetflixIndexComponent";
+import RegisterComponent from "./components/RegisterComponent";
+import DataBindingComponent from "./components/DataBindingComponent";
+import ShoppingComponent from "./components/ShoppingComponent";
+import EventBindingComponent from "./components/EventBindingComponent";
+import TwoWayBindingComponent from "./components/TwoWayBindingComponent";
+import TwoWayClassDemo1 from "./components/TwoWayClassDemo1";
+import TwoWayClassDemo from "./components/TwoWayClassDemo";
+import reportWebVitals from "./reportWebVitals";
+import LoginComponent from "./components/LoginComponent";
+import ShoppingClassDemo from "./components/ShoppingClassDemo";
+import FormComponent from "./components/FormComponent";
+import FormikDemo from "./components/FormikDemo";
+import FormikValidation from "./components/FormikValidation";
+import YupValidation from "./components/YupValidation";
+import YupValidationComponent from "./components/YupValidationComponent";
+import LifeCycleDemo from "./components/LifeCycleDemo";
+import ReactHookDemo from "./components/ReactHookDemo";
+import ContextDemo from "./components/ContextDemo";
+import UserLogin from "./components/UserLogin";
+import { CookiesProvider } from "react-cookie";
+import ReducerDemo from "./components/ReducerDemo";
+import CustomHookDemo from "./components/CustomHookDemo";
+import SentenceCase from "./components/SentenceCase";
+import JQueryAjaxDemo from "./components/JQueryAjaxDemo";
+import AxiosDemo from "./components/AxiosDemo";
+import ShoppingIndex from "./shopping/ShoppingIndex";
+import SPAComponent from "./components/SPAComponent";
+import IShopIndex from "./ishop/IShopIndex";
+import MainComponent from "./BankApp/MainComponent";
+import SerachDemo from "./components/SearchDemo";
+import NetflixHeaderComponent from "./components/NetflixHeaderComponent";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <FormComponent />
   </React.StrictMode>
 );
 
